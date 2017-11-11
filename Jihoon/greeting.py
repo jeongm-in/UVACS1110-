50a32e028af182b399324123ebe4011958dcc965
Bartleby2718 <bartleby2718@gmail.com>
2017-11-11 17:25:54 +0900

    created my folder and finished the first assignment

---
+++ Jihoon/greeting.py
@@ -0,0 +1,5 @@
+intro = "Hi, I'm Jihoon Park."
+fun_fact = 'I like playing a computer game that was first released 20 years ago--Starcraft: Brood War.'
+print(intro)
+print(fun_fact)
+# I started programming when I entered UVA.
