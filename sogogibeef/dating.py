d72cde0ec296601e704f770fe2754b008e0f163d
Jeong Min Lim <limjeongmin@wustl.edu>
2017-11-12 01:29:47 +0900

    changed the file to use function. WIP

--- sogogibeef/dating.py
+++ sogogibeef/dating.py
@@ -5,14 +5,29 @@
 # as old as twice your age minus 13
 
 
+# plus
+def date_age_range_calculator():
+    user_age = input('How old are you? ') # apparently input returns string value.
+    user_age_in_int = int(user_age) # casts user_age String data type to integer data type
+    if user_age_in_int < 14:
+        print('Error; You should be older than 13. ', end='')
+        date_age_range_calculator()
+        # Q: what is general opinion on calling a function within itself?
+        # Is this even recursion in the first place?
+        # When I prompt error message and input expected age, calculation breaks.
+        # Should check again here later.
 
-user_age = input('How old are you? ')
-# apparently input returns string value.
-user_age_in_int = int(user_age) #casts user_age String data type to integer data type
-date_lower_limt = user_age_in_int / 2 + 7
-date_upper_limit = user_age_in_int * 2 - 13
+    return user_age_in_int
 
-print('You can date people between %d and %d years old' % (date_lower_limt, date_upper_limit))
+def age_limit_calculator(age_of_user):
+    date_lower_limt = age_of_user / 2 + 7
+    date_upper_limit = age_of_user * 2 - 13
+    print('You can date people between %d and %d years old' % (date_lower_limt, date_upper_limit), end='\n')
+
+
+your_age = date_age_range_calculator()
+age_limit_calculator(your_age)
+
 
 
 
