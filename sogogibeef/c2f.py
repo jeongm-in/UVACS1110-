4cd2d1d32e63afa291708fb1d2a2bda68aba2066
Jeong Min Lim <limjeongmin@wustl.edu>
2017-11-12 17:27:49 +0900

    changed formatting method

--- sogogibeef/c2f.py
+++ sogogibeef/c2f.py
@@ -3,7 +3,7 @@
 # and prints the corresponding temperature in Fahrenheit.
 
 celsius_temperature = float(input('What is the temperature in Celsius? '))
-fahrenheit_temperature = (celsius_temperature * 9 / 5.0 + 32)
+fahrenheit_temperature = (celsius_temperature * 9 / 5 + 32)
 print('It is %.1f degrees Fahrenheit' % fahrenheit_temperature)
 # % .nf : determine which digit to round.
 # Source : https://stackoverflow.com/a/455678/8776165
