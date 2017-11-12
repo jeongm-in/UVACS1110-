060b35ef6d830e4dbccc745b4e427d992e9d2f96
Jeong Min Lim <limjeongmin@wustl.edu>
2017-11-12 12:08:31 +0900

    Debugged using while loop

--- sogogibeef/dating.py
+++ sogogibeef/dating.py
@@ -5,24 +5,19 @@
 # as old as twice your age minus 13
 
 
-# plus
+# Plus
 def date_age_range_calculator():
-    user_age = input('How old are you? ') # apparently input returns string value.
-    user_age_in_int = int(user_age) # casts user_age String data type to integer data type
-    if user_age_in_int < 14:
+    user_age = int(input('How old are you? ')) # Note to self : Apparently input returns string value.
+    while user_age < 14:
         print('Error; You should be older than 13. ', end='')
-        date_age_range_calculator()
-        # Q: what is general opinion on calling a function within itself?
-        # Is this even recursion in the first place?
-        # When I prompt error message and input expected age, calculation breaks.
-        # Should check again here later.
+        user_age = int(input('How old are you? '))
 
-    return user_age_in_int
+    return user_age
 
 def age_limit_calculator(age_of_user):
     date_lower_limt = age_of_user / 2 + 7
     date_upper_limit = age_of_user * 2 - 13
-    print('You can date people between %d and %d years old' % (date_lower_limt, date_upper_limit), end='\n')
+    print('You can date people between %d and %d years old' % (date_lower_limt, date_upper_limit))
 
 
 your_age = date_age_range_calculator()
