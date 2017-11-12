59f65f36ec475cb0bdeb7bec4a5f7b2122f0ced7
Jeong Min Lim <limjeongmin@wustl.edu>
2017-11-12 10:45:51 +0900

    fixed follwing ybartleby's advice

--- sogogibeef/nonsense.py
+++ sogogibeef/nonsense.py
@@ -4,13 +4,10 @@
 # then print the following string : "word doesn't even sound like a word anymore"
 
 
-def repeat_12_Times(word):
-    list_of_words = [''] * 12
-    for i in range(12):
-        list_of_words[i] = word
+def repeat_12_times(word):
+    list_of_words = [word] * 12 # Following bartleby's advice, removed unnecessary iteration
     word_repeated = ' '.join(list_of_words)
     print(word_repeated)
-
     print(('\"%s\" doesn\'t even sound like a word anymore') % word)
     # following bartleby's advice, used %s string formatter instead of messy escape sequences
 
@@ -19,4 +16,4 @@ favorite_word = input("Please type your favorite word: ")
 # I am so used to initializing variables. Perhaps (at least for me) biggest difference
 # Q : for long comments, what should I do? break to the next line?
 
-repeat_12_Times(favorite_word)
+repeat_12_times(favorite_word)
