6da7d62881695c13f0b91a59110b36a03532ae88
Jeong Min Lim <limjeongmin@wustl.edu>
2017-11-13 14:01:20 +0900

    Finished assingment 07 quadratic.py. I did learn how to solve cubic and quadratic equations, but I do not remember how. Ouch

---
+++ sogogibeef/quadratic.py
@@ -0,0 +1,14 @@
+# Version: Jeong Min Lim (sogogibeef)
+# quadratic.py contains big_root function that takes three input
+# and returns the more positive root of the quadratic equation.
+
+# a*x**2 + b*x + c
+
+def big_root(a, b, c):
+    bigger_root = (-b + (b ** 2 - 4 * a * c) ** (1 / 2)) / (2 * a)
+    return bigger_root
+
+def small_root(a, b, c):
+    smaller_root = (-b - (b ** 2 - 4 * a * c) ** (1 / 2)) / (2 * a)
+    return smaller_root
+
