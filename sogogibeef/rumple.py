d08b1ffa5db27c55cdeeb95261cb8ac078c3ecdf
Jeong Min Lim <limjeongmin@wustl.edu>
2017-11-24 20:46:20 +0900

    Reformatted and fixed minor typo

--- sogogibeef/rumple.py
+++ sogogibeef/rumple.py
@@ -7,16 +7,16 @@ print('You will never win the game, for Rumpelstiltskin is my name.')
 guesses = 0
 
 while is_correct_guess == False:
-    if guesses  == 500: # TQ1: limit number of guesses
+    if guesses == 500:  # TQ1: limit number of guesses
         break
     user_answer = input('Guess my name: ')
     if user_answer == answer:
-        break #
-    elif user_answer !='': # TQ2: Not to gloat when user inputs nothing
+        break  #
+    elif user_answer != '':  # TQ2: Not to gloat when user inputs nothing
         print('Ha! You\'ll never guess!')
     guesses += 1
 
-if guesses < 5:
+if guesses < 500:
     print('No! How did you guess?')
 else:
     print('I told you couldn\'t guess!')
