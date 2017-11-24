5a3ba2faf20b39db4a7acb09621e4c321f2232bf
Jeong Min Lim <limjeongmin@wustl.edu>
2017-11-24 20:26:37 +0900

    No file was added in last commit

---
+++ sogogibeef/rumple.py
@@ -0,0 +1,22 @@
+# Version : Jeong-Min Lim(sogogibeef)
+# rumple.py prompts user to guess correct name.
+
+is_correct_guess = False
+answer = 'Rumpelstiltskin'
+print('You will never win the game, for Rumpelstiltskin is my name.')
+guesses = 0
+
+while is_correct_guess == False:
+    if guesses  == 500: # TQ1: limit number of guesses
+        break
+    user_answer = input('Guess my name: ')
+    if user_answer == answer:
+        break #
+    elif user_answer !='': # TQ2: Not to gloat when user inputs nothing
+        print('Ha! You\'ll never guess!')
+    guesses += 1
+
+if guesses < 5:
+    print('No! How did you guess?')
+else:
+    print('I told you couldn\'t guess!')
