375c17b4815ec9cac18dc0cd7b91415b44b932f5
sogogibeef <limjeongmin@wustl.edu>
2017-12-01 23:53:53 +0900

    Delete letter2Bart.py
    I'll use issue.

--- Jihoon/letter2Bart.py
+++
@@ -1,8 +0,0 @@
-# Hi. I am at Shenzhen right now.
-# Too bad I didn't know Facebook, Twitter, Instagram, Google are all banned in China.
-# Nothing I can do without those services, but luckily github works.
-# But again, I forgot to push my progress to master before I left for China
-# Guess I'll have to re-do stuff from scratch here.
-# Between Professors, Assistant Professors, and Graudate students who all know each other well
-# There is no place for trifle undergraduate.
-# So awkward lol
