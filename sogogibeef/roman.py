1dd6bcc8e96ff24bbb855f77648993c1492b1b75
Jeong-Min Lim <limjeongmin@wustl.edu>
2017-12-19 23:11:00 +0900

    Finished roman.py. still need to work on simplifying the code

--- sogogibeef/roman.py
+++ sogogibeef/roman.py
@@ -12,13 +12,27 @@ if arabic_numeral <= lower_limit or arabic_numeral >= upper_limit:
     print('Input must be between 1 and 3999.')
 
 else:
-
     for denominator in roman_dictionary.keys():
-        # print(denominator)
         how_many_roman_numerals = divmod(arabic_numeral, denominator)
         for num in range(how_many_roman_numerals[0]):
             roman_numeric_list.append(roman_dictionary[denominator])
         arabic_numeral = how_many_roman_numerals[1]  # #15
 
-    roman_numeric_result = ''.join(roman_numeric_list)
-    print(roman_numeric_result)
+    roman_numeric_joined = ''.join(roman_numeric_list)
+
+# Fix roman numerics in proper format
+# TODO : how to extract if bulk for formatting
+if 'DCCCC' in roman_numeric_joined:
+    roman_numeric_joined = roman_numeric_joined.replace('DCCCC', 'CM')
+if 'D' not in roman_numeric_joined and 'CCCC' in roman_numeric_joined:
+    roman_numeric_joined = roman_numeric_joined.replace('CCCC', 'CD')
+if 'LXXXX' in roman_numeric_joined:
+    roman_numeric_joined = roman_numeric_joined.replace('LXXXX', 'XC')
+if 'L' not in roman_numeric_joined and 'XXXX' in roman_numeric_joined:
+    roman_numeric_joined = roman_numeric_joined.replace('XXXX', 'XL')
+if 'VIIII' in roman_numeric_joined:
+    roman_numeric_joined = roman_numeric_joined.replace('VIIII', 'IX')
+if 'V' not in roman_numeric_joined and 'IIII' in roman_numeric_joined:
+    roman_numeric_joined = roman_numeric_joined.replace('IIII', 'IV')
+
+print(roman_numeric_joined)
