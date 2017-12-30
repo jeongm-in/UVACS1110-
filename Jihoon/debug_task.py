50427b3adb1d4f2f2ef245ab4be3f5ccc6c6b3bb
Bartleby2718 <bartleby2718@gmail.com>
2017-12-30 18:10:16 +0900

    Finished Lab 9: Debugging

--- Jihoon/debug_task.py
+++ Jihoon/debug_task.py
@@ -8,33 +8,38 @@ def pow2(n):
     """Computes are returns the largest power of two that is no larger than n"""
     ans = 1
     while ans*2 <= n:
-        ans * 2
+        ans *= 2
     return ans
 
 print("The Game of Nim\n")
 while marbles <= 0:
-    marbles = input("The number of marbles in the pile: ")
+    marbles = int(input("The number of marbles in the pile: "))
 turn_choice = input("Who will start? (p or c): ")
 turn = False
 if turn_choice == 'p':
     turn = True
 
 while marbles != 0:
-    print("The pile has", marbles, "marbles in it.")
+    print("The pile has ", marbles, " marbles in it.")
     if turn:
         can_take = marbles // 2
-        take = 0
-        while take > can_take:
-            take = int(input("How many marbles to you want to take (1-" + str(can_take) + "): "))
+        if can_take == 0:
+            take = 1
+        else:
+            take = 0
+            while take > can_take or take < 1:
+                take = int(input(
+                    "How many marbles to you want to take (1-" + str(
+                        can_take) + "): "))
         marbles -= take
     else:
         take = 1
-        target = pow2(marbles)
+        target = pow2(marbles) - 1
         take = marbles - target
-        if take < 1:
+        if take < 1 or take > marbles // 2:
             take = 1
         marbles -= take
-        print("The computer takes", take, "marbles.")
+        print("The computer takes ", take, " marbles.")
 
     turn = not turn
 
