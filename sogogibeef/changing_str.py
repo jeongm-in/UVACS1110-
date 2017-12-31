0180adb32b76280f80af3ee69881e03870c1906a
Jeong-Min Lim <limjeongmin@wustl.edu>
2017-12-31 12:01:25 +0900

    finished string Lab

---
+++ sogogibeef/changing_str.py
@@ -0,0 +1,4 @@
+a = "reasonable simplicity"
+new_str = a[0] + a[2] + 'd' + a[12] + a[-3] + a[2] + a[8] + a[10] + a[17] + a[4] + a[13] + a[14] + a[15] + a[1] + 'x' \
+          + a[-3] + a[-2] + a[-1]
+print(new_str)
