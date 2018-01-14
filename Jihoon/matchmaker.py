605dd4d8f64302e6180be162710e84303b802fd3
Bartleby2718 <bartleby2718@gmail.com>
2018-01-14 16:08:52 +0900

    Split tests and created a new file. Close #16

--- Jihoon/matchmaker.py
+++ Jihoon/matchmaker.py
@@ -1,6 +1,3 @@
-import unittest
-
-
 def agreement(i1, i2):
     return [x for x in i1 if x in i2]
 
@@ -36,62 +33,3 @@ def bestmatch(me, others):
             comp = match
             whom = name
     return whom
-
-
-class TestMatchmaker(unittest.TestCase):
-    def test_agreement(self):
-        i1 = ['seocho', 'gangnam', 'yeoksam']
-        i2 = ['yeoksam', 'samseong', 'jamsil']
-        self.assertListEqual(agreement(i1, i2), ['yeoksam'])
-
-    def test_disagreement(self):
-        i1 = ['seocho', 'gangnam', 'yeoksam']
-        i2 = ['yeoksam', 'samseong', 'jamsil']
-        self.assertListEqual(disagreement(i1, i2),
-                             ['seocho', 'gangnam', 'samseong', 'jamsil'])
-
-    def test_compatibility(self):
-        i1 = ['seocho', 'gangnam', 'yeoksam']
-        i2 = ['yeoksam', 'samseong', 'jamsil']
-        self.assertEqual(compatibility(i1, i2), .2)
-
-    def test_bestmatch(self):
-        me = ['coding', 'homework', 'money', 'reciting poetry']
-
-        k = ['homework']
-        m = ['coding', 'homework', 'sleep']
-        d = ['money', 'cruises', 'reciting poetry', 'gardening']
-
-        match = bestmatch(me, [['K.', k], ['M.', m], ['D.', d]])
-        self.assertEqual(match, 'M.')
-
-    def test_first_three_with_tuples(self):
-        a = ('gam', 'nyong', 'jock')
-        b = ('ting', 'nyong', 'gam')
-        self.assertListEqual(agreement(a, b), ['gam', 'nyong'])
-        self.assertListEqual(disagreement(a, b), ['jock', 'ting'])
-        self.assertEqual(compatibility(a, b), .5)
-
-    def test_first_three_with_empty_lists(self):
-        a = ('gam', 'nyong', 'jock', 'ting')
-        b = []
-        self.assertListEqual(agreement(a, b), [])
-        self.assertListEqual(disagreement(a, b),
-                             ['gam', 'nyong', 'jock', 'ting'])
-        self.assertEqual(compatibility(a, b), 0)
-
-    def test_first_three_with_same_lists(self):
-        a = ('gam', 'nyong', 'jock', 'ting')
-        b = ['gam', 'nyong', 'jock', 'ting']
-        self.assertListEqual(agreement(a, b),
-                             ['gam', 'nyong', 'jock', 'ting'])
-        self.assertListEqual(disagreement(a, b), [])
-        self.assertEqual(compatibility(a, b), 1)
-
-    def test_first_three_with_mutually_disjoint_lists(self):
-        a = ('gam', 'nyong')
-        b = ['jock', 'ting']
-        self.assertListEqual(agreement(a, b), [])
-        self.assertListEqual(disagreement(a, b),
-                             ['gam', 'nyong', 'jock', 'ting'])
-        self.assertEqual(compatibility(a, b), 0)
